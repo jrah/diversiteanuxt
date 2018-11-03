80b693193598d91e28c39e1fb2e95747435d0489
jrah <jjames.home@gmail.com>
2018-11-03 00:23:42 +0000

    Update for new tailwind media queries content

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,41 @@
 <template>
-  <section class="">
+  <section class="container">
     <form class="w-full max-w-xs">
-      <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3">
+      <div class="ns:flex ns:items-center mb-6">
+        <div class="ns:w-1/3">
           <label
-            class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4"
+            class="block text-grey font-bold ns:text-right mb-1 ns:mb-0 pr-4"
             for="inline-full-name">
             Full Name
           </label>
         </div>
-        <div class="md:w-2/3">
+        <div class="ns:w-2/3">
           <input
             id="inline-full-name"
-            class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple"
+            class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-navy"
             type="text"
             value="Jane Doe">
         </div>
       </div>
-      <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3">
+      <div class="ns:flex ns:items-center mb-6">
+        <div class="ns:w-1/3">
           <label
-            class="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4"
+            class="block text-grey font-bold ns:text-right mb-1 ns:mb-0 pr-4"
             for="inline-username">
             Password
           </label>
         </div>
-        <div class="md:w-2/3">
+        <div class="ns:w-2/3">
           <input
             id="inline-username"
-            class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple"
+            class="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-navy"
             type="password"
             placeholder="******************">
         </div>
       </div>
-      <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3"/>
-        <label class="md:w-2/3 block text-grey font-bold">
+      <div class="ns:flex ns:items-center mb-6">
+        <div class="ns:w-1/3"/>
+        <label class="ns:w-2/3 block text-grey font-bold">
           <input
             class="mr-2 leading-tight"
             type="checkbox">
@@ -44,11 +44,11 @@
           </span>
         </label>
       </div>
-      <div class="md:flex md:items-center">
-        <div class="md:w-1/3"/>
-        <div class="md:w-2/3">
+      <div class="ns:flex ns:items-center">
+        <div class="ns:w-1/3"/>
+        <div class="ns:w-2/3">
           <button
-            class="shadow bg-purple hover:bg-purple-light focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            class="shadow bg-navy hover:bg-navy-light focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
             type="button">
             Sign Up
           </button>
