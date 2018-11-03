a636eeb795bf12aee122129ed4e4376b5e70595b
jrah <jjames.home@gmail.com>
2018-11-03 03:39:58 +0000

    Add next/back buttons

--- pages/index.vue
+++ pages/index.vue
@@ -9,12 +9,14 @@
         <div
           style="background-image: url('https://source.unsplash.com/gy_DN08336U')"
           class="bg-black bg-cover ns:w-1/4 h-full rounded-l-lg"/>
-        <form class="bg-grey-lightest ns:w-3/4 p-4 rounded-r-lg">
-          <div class="p-5">
-            <h1 class="leading-tight">Lorem ipsum dolor sit amet.</h1>
-            <p class="leading-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum debitis dolore maxime voluptate dolorem blanditiis?</p>
-
+        <form class="bg-grey-lightest ns:w-3/4 p-5 rounded-r-lg">
+          <div class="">
+            <h1 class="leading-tight mt-0 mb-3">Lorem ipsum dolor sit amet.</h1>
+            <p class="leading-normal mt-0 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum debitis dolore maxime voluptate dolorem blanditiis?</p>
           </div>
+
+          <button class="text-navy hover:text-navy-light font-bold py-2 px-4">Back</button>
+          <button class="bg-navy hover:bg-navy-dark text-white font-bold py-2 px-4 rounded">Next</button>
 
         </form>
 
