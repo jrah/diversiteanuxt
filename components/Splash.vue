7f62379411eb46b2a1d80b2981b20cc3344bec86
jrah <jjames.home@gmail.com>
2018-11-08 19:56:14 +0000

    Increase max width content for splash

--- components/Splash.vue
+++ components/Splash.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <section class="py-12 bg-grey-lightest text-navy">
     <div class="container">
-      <div class="max-w-xs m-auto text-center">
+      <div class="max-w-md m-auto text-center">
         <h1 class="mt-0 mb-2 font-bold text-4xl leading-tight">{{ home.splash.title }}</h1>
         <p class="leading-normal mt-0">{{ home.splash.text }}</p>
       </div>
