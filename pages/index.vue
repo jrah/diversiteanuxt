eb84155d45d58859ca2e764e263ed169368a24f2
jrah <jjames.home@gmail.com>
2018-11-08 16:44:29 +0000

    Add svg append

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
             :key="index"
             :name=" '' + index "
             class="text-center ns:w-1/4">
-            <img :src="'~/assets/images/' + block.icon"  alt="block.icon">
+            <img :src="'~/assets/images/' + block.icon + '.svg'"  :alt="block.icon">
             <h2 class="leading-tight text-lg mt-1 mb-1">{{ block.title }}</h2>
             <p class="leading-normal mt-0 mb-6">{{ block.paragraph }}</p>
           </div>
