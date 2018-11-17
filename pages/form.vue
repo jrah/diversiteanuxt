7200b9dec4ed204b3b4afc5a197ce7955a6da393
jrah <jjames.home@gmail.com>
2018-11-17 17:59:36 +0000

    Loop through questions and inputs

--- pages/form.vue
+++ pages/form.vue
@@ -1,10 +1,12 @@
 <template>
   <section class="h-screen bg-navy flex items-center">
     <div
+      v-for="(question, index) in quiz.questions"
+      :key="index"
       class="max-w-lg mx-auto"
       style="max-height: 28rem; height: 95%; width:95%;">
       <!-- <h1 class="text-grey-darker text-base">Introduction..</h1> -->
-      <h1 class="text-grey-dark font-light leading-tight text-xl mb-3">Are you adventurous?</h1>
+      <h1 class="text-grey-dark font-light leading-tight text-xl mb-3">{{ question.heading }}</h1>
       <div class="ns:rounded-lg ns:shadow-md flex ns:h-full">
         <div
           style="background-image: url('https://source.unsplash.com/gy_DN08336U')"
@@ -12,15 +14,15 @@
         <form class="bg-grey-lightest ns:w-3/4 p-5 rounded-r-lg relative">
           <div
             class="text-grey-darkest">
-            <h1 class="leading-tight mt-0 mb-3">Lorem ipsum dolor sit amet.</h1>
-            <p class="leading-normal mt-0 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum debitis dolore maxime voluptate dolorem blanditiis?</p>
-            <div class="mb-2">
-              <div class="ns:w-1/3"/>
-              <radio value="First"/>
-            </div>
-            <div class="mb-2">
-              <div class="ns:w-1/3"/>
-              <radio value="Second"/>
+            <h2 class="leading-tight mt-0 mb-3">{{ question.subheading }}</h2>
+            <p class="leading-normal mt-0 mb-3">{{ question.paragraph }}</p>
+            <div 
+              v-for="(input, index) in question.inputs" 
+              :key="index">
+              <div class="mb-2">
+                <div class="ns:w-1/3"/>
+                <radio :value="input.value"/>
+              </div>
             </div>
             <div class="absolute pin-b pin-r p-3">
               <button
@@ -56,7 +58,7 @@ export default {
   data() {
     return {
       questionIndex: 0,
-      userResponses: Array(),
+      userInputs: Array(),
       quiz: quiz
     }
   },
@@ -65,18 +67,18 @@ export default {
     next: function() {
       this.questionIndex++
       // eslint-no-console
-      // console.log(this.userResponses)
+      // console.log(this.userInputs)
     },
     prev: function() {
       this.questionIndex--
     },
     score: function() {
       const modeMap = {}
-      const maxEl = this.userResponses[0]
-      this.userResponses.forEach((userResponse, index) => {
+      const maxEl = this.userInputs[0]
+      this.userInputs.forEach((userInput, index) => {
         // eslint-no-console
-        console.log(userResponse)
-        let el = this.userResponses[index]
+        console.log(userInput)
+        let el = this.userInputs[index]
         if (modeMap[el] == null) {
           modeMap[el] = 1
         } else {
