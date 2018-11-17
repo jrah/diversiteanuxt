eddc728d5ae70bb1040ae702261d89d8323d61b6
jrah <jjames.home@gmail.com>
2018-11-17 16:44:30 +0000

    Add basic form logic

--- pages/form.vue
+++ pages/form.vue
@@ -10,7 +10,8 @@
           style="background-image: url('https://source.unsplash.com/gy_DN08336U')"
           class="bg-black bg-cover ns:w-1/4 h-full rounded-l-lg"/>
         <form class="bg-grey-lightest ns:w-3/4 p-5 rounded-r-lg relative">
-          <div class="text-grey-darkest">
+          <div
+            class="text-grey-darkest">
             <h1 class="leading-tight mt-0 mb-3">Lorem ipsum dolor sit amet.</h1>
             <p class="leading-normal mt-0 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum debitis dolore maxime voluptate dolorem blanditiis?</p>
             <div class="mb-2">
@@ -22,8 +23,13 @@
               <radio value="Second"/>
             </div>
             <div class="absolute pin-b pin-r p-3">
-              <button class="text-navy hover:text-navy-light font-bold py-2 px-4">Back</button>
-              <button class="bg-navy hover:bg-navy-dark text-white font-bold py-2 px-4 rounded">Next</button>
+              <button
+                v-if="questionIndex > 0"
+                class="text-navy hover:text-navy-light font-bold py-2 px-4"
+                @click="prev">Back</button>
+              <button
+                class="bg-navy hover:bg-navy-dark text-white font-bold py-2 px-4 rounded"
+                @click="next">Next</button>
             </div>
           </div>
 
@@ -38,61 +44,49 @@
 
 <script>
 import Radio from '~/components/Radio.vue'
+import Questions from '~/content/questions.json'
 
+const quiz = Questions
+console.log(quiz)
 export default {
   components: {
     Radio
   },
-  props: {
-    picked: {
-      type: String,
-      required: true
-    }
-  },
+  props: {},
   data() {
     return {
-      questions: [
-        {
-          text: 'How adventurous are you?',
-          responses: [
-            {
-              text: 'Not adventurous',
-              value: 'Type 1',
-              type: 'checkbox'
-            },
-            {
-              text: 'adventurous',
-              value: 'Type 2',
-              type: 'checkbox'
-            },
-            {
-              text: 'Somewhat Adventurous',
-              value: 'Type 3',
-              type: 'checkbox'
-            }
-          ]
-        },
-        {
-          text: 'What best describes your tea experience?',
-          responses: [
-            {
-              text: 'I am new to the tea world',
-              value: 'Type 1',
-              type: 'checkbox'
-            },
-            {
-              text: 'I am a tea enthusiast',
-              value: 'Type 2',
-              type: 'checkbox'
-            },
-            {
-              text: 'For popularity',
-              value: 'Type 3',
-              type: 'checkbox'
-            }
-          ]
+      questionIndex: 0,
+      userResponses: Array(),
+      quiz: quiz
+    }
+  },
+  methods: {
+    // go to next question
+    next: function() {
+      this.questionIndex++
+      // eslint-no-console
+      // console.log(this.userResponses)
+    },
+    prev: function() {
+      this.questionIndex--
+    },
+    score: function() {
+      const modeMap = {}
+      const maxEl = this.userResponses[0]
+      this.userResponses.forEach((userResponse, index) => {
+        // eslint-no-console
+        console.log(userResponse)
+        let el = this.userResponses[index]
+        if (modeMap[el] == null) {
+          modeMap[el] = 1
+        } else {
+          modeMap[el]++
         }
-      ]
+        if (modeMap[el] > maxCount) {
+          maxEl = el
+          maxCount = modeMap[el]
+        }
+      })
     }
   }
 }
