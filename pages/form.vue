ffa09ef3a0080be73d01a40ff5e0a44ac9a6c651
jrah <jjames.home@gmail.com>
2018-11-18 15:15:12 +0000

    Remove form element

--- pages/form.vue
+++ pages/form.vue
@@ -7,12 +7,20 @@
         Ask to try a sample tea or do full subscription
         Confirmation of either sent to us via firebase or email
         Payment goes through snipcart
+        Allow for both checkbox and text input type
+
+      Bugs:
+        Different input selection should replace in array, currently only seems to append array, unless check box or otherwise
+        When click next seems to go to the next one but then back again
+        When click on one radio button/checkbox corresponding is also clicked. Could be to do with the index name of each question set.
+
+          https://codepen.io/heatherduvall/pen/ggedwy
   -->
   <section
     v-cloak
     class="h-screen bg-navy flex items-center">
     <div
-      v-for="(question, index) in quiz.questions"
+
       :key="index"
       class="max-w-lg mx-auto"
       style="max-height: 28rem; height: 95%; width:95%;">
@@ -27,45 +35,47 @@
           <p>This is the current index</p>
         </div>
       </div>
-      <!-- <h1 class="text-grey-darker text-base">Introduction..</h1> -->
-      <h1 class="text-grey-dark font-light leading-tight text-xl mb-3">{{ question.heading }}</h1>
       <div class="ns:rounded-lg ns:shadow-md flex ns:h-full">
         <div
           style="background-image: url('https://source.unsplash.com/gy_DN08336U')"
           class="bg-black bg-cover ns:w-1/4 h-full rounded-l-lg"/>
-        <form class="bg-grey-lightest ns:w-3/4 p-5 rounded-r-lg relative">
+        <div class="bg-grey-lightest ns:w-3/4 p-5 rounded-r-lg relative">
           <div
-            class="text-grey-darkest">
-            <h2 class="leading-tight mt-0 mb-3">{{ question.subheading }}</h2>
-            <p class="leading-normal mt-0 mb-3">{{ question.paragraph }}</p>
+            v-for="(question, index) in quiz.questions"
+            :key="index">
+            <h1 class="text-grey-dark font-light leading-tight text-xl mb-3">{{ question.heading }}</h1>
             <div
-              v-for="(input, index) in question.inputs"
-              :key="index">
-              <div class="mb-2">
+              class="text-grey-darkest">
+              <h2 class="leading-tight mt-0 mb-3">{{ question.subheading }}</h2>
+              <p class="leading-normal mt-0 mb-3">{{ question.paragraph }}</p>
+              <div
+                v-for="(input, index) in question.inputs"
+                :key="index"
+                class="mb-2">
                 <div class="ns:w-1/3"/>
-                <radio :value="input.value"/>
+                <label>
+                  <input
+                    :value="input.value"
+                    v-model="userInputs[index]"
+                    :name="index"
+                    type="radio"> {{ input.text }}
+                </label>
+                <!-- <radio :value="input.value"/> -->
+              </div>
+              <div class="absolute pin-b pin-r p-3">
+                <button
+                  v-if="questionIndex > 0"
+                  class="text-navy hover:text-navy-light font-bold py-2 px-4"
+                  @click="prev">Back</button>
+                <button
+                  class="bg-navy hover:bg-navy-dark text-white font-bold py-2 px-4 rounded"
+                  @click="next">Next</button>
               </div>
             </div>
-            <div class="absolute pin-b pin-r p-3">
-              <button
-                v-if="questionIndex > 0"
-                class="text-navy hover:text-navy-light font-bold py-2 px-4"
-                @click="prev">Back</button>
-              <button
-                class="bg-navy hover:bg-navy-dark text-white font-bold py-2 px-4 rounded"
-                @click="next">Next</button>
-            </div>
-          </div>
-
-          <div v-show="questionIndex === quiz.questions.length">
-            <h3>Your Results</h3>
-            <p>
-              You are: {{ score() }}
-            </p>
           </div>
 
 
-        </form>
+        </div>
 
       </div>
     </div>
@@ -100,24 +110,6 @@ export default {
     },
     prev: function() {
       this.questionIndex--
-    },
-    score: function() {
-      const modeMap = {}
-      const maxEl = this.userInputs[0]
-      this.userInputs.forEach((userInput, index) => {
-        // eslint-no-console
-        console.log(userInput)
-        let el = this.userInputs[index]
-        if (modeMap[el] == null) {
-          modeMap[el] = 1
-        } else {
-          modeMap[el]++
-        }
-        if (modeMap[el] > maxCount) {
-          maxEl = el
-          maxCount = modeMap[el]
-        }
-      })
     }
   }
 }
