891c3a1248cb4912fc10a0300e3cffbab15ba577
jrah <jjames.home@gmail.com>
2018-12-02 02:38:21 +0000

    Add varied inputs

--- components/Question.vue
+++ components/Question.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-    <h2 class="leading-tight mt-0 mb-3"/>
-    <p class="leading-normal mt-0 mb-3">{{ question.paragraph }}</p>
     <div
       v-for="(input, index) in question.inputs"
       :key="index"
       class="mb-2">
-      <div class="ns:w-1/3"/>
-      <label>
+      <div v-if="input.type == 'radio'">
+        <div class="ns:w-1/3"/>
+        <label>
+          <input
+            :value="index"
+            :name="questionIndex"
+            :checked="isChecked(index)"
+            :type="input.type"
+            @change="handleChange(input, index)"> {{ input.text }}
+        </label>
+      </div>
+      <div v-else>
+        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">{{ input.text }}</label>
         <input
-          :value="index"
           :name="questionIndex"
           :checked="isChecked(index)"
           :type="input.type"
-          @change="handleChange(index)"> {{ input.text }}
-
-      </label>
+          :value="input.text"
+          class="appearance-none block bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-grey"
+          @change="handleChange(input, index)">
+      </div>
       <!-- <radio :value="input.value"/> -->
 
     </div>
@@ -45,8 +54,12 @@ export default {
     isChecked(index) {
       return this.$store.getters['quiz/isChecked'](index)
     },
-    handleChange(inputIndex) {
-      this.$store.commit('quiz/UPDATE_QUESTION', { selectedInput: inputIndedx })
+    handleChange(input, index) {
+      const selectedInput = {
+        ...input,
+        index
+      }
+      this.$store.commit('quiz/UPDATE_QUESTION', { selectedInput })
     }
   }
 }
